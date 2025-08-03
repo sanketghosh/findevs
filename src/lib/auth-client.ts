651692ae23e0ee459d55f29c0bb72cb15599ff82
sanket <code.sanket13@gmail.com>
2025-08-03 23:50:38 +0530

    add: better-auth client baseurl fix

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -3,6 +3,6 @@ import { adminClient } from "better-auth/client/plugins";
 
 export const authClient = createAuthClient({
   //you can pass client configuration here
-  baseURL: "http://localhost:3000",
+  baseURL: process.env.NEXT_PUBLIC_BETTER_AUTH_URL,
   plugins: [adminClient()],
 });
